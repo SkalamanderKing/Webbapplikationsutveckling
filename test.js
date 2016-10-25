63d2a83a48be1c58fea33026dd38becc4121a005
SkalamanderKing <skala_mander@yahoo.se>
2016-10-25 19:45:44 +0200

    nytt

--- test.js
+++ test.js
@@ -6,144 +6,111 @@ var input2;
 var gissa;
 var rand;
 
-//Blir sann om spelaren spelat 1 ggr
-var nyttSpel=false;
-
+//nyGiss kontrollerar spelarens 2 första inmatningar, som blir sann när allt är bra/kontrollerat
+var nyGiss=false;
 //Så länge denna är sann pågår spelet
-var start=true;
+var start;
 
 //Poäng räknare
 var f =0;
 var r=0;
+var w1=true;
+var w2=true;
+var w3=true;
 
 /*"spelloopen", en loop pågår till spelaren avslutar*/
 function spelloop() {
 	
-	//1:a pop-up
+	//Intro
 	start=window.confirm("Välj två siffor mellan 1-100 som du vill spela med.");
+	if(!start){location.reload(true); return;}
 	
-	//om spelaren trycker Cancel körs Sluta-funktionen
-	if(start==false){sluta();}
-	
-	//Denna funktion kollar diverse felinmatningar av spelaren
-	 kontroll();
-	 
-			while(start){
-				
-				//konverta string till nummer
-				input1=Number(input1);
-				input2=Number(input2);
-				gissa=Number(gissa);
-	
-				if(start==false){sluta();}
-				
-				//Vid nästa rundas spel körs inte denna slump
-				if(!nyttSpel)
+	while(true){
+			if(!nyGiss){
+				//input är spelarens första siffra, inData är en kontroll-metod
+				input1=inData();
+				if(input1==null){location.reload(true); return;}
+				input2=inData();
+				if(input2==null){location.reload(true); return;}
+				//Om input1 och 2 är ok skickas de till slumpgenerator
 				rand=randomIntFromInterval(input1, input2);
-	
-			//spelaren gissade rätt dvs inmatningen var densamma som sluptalet
-			if (gissa===rand)
-			{
-						//Nollställa alla variabler
-						gissa=null;
-						input1=null;
-						input2=null;
-						rand=null;
-						
-						//Poäng
-						r=r+1;
-						
-						//presentera rätt och ev fel
-						alert("RÄTT! Antal rätt: " +r +" Antal fel: "+ f +" "); //+rand
-						
-						//Nytt spel blir tmp sann
-						var tmp=window.confirm("Tryck OK för att spela igen!");
-				
-				if(tmp)
-				{
-						//sättes till sann så slumpgeneratorn tidigare inte körs
-						nyttSpel=true;
-						
-				
-						
-						//Och ny kontroll
-						kontroll();
-						
-								//nytt slumptal
-						rand=randomIntFromInterval(input1, input2);
+				nyGiss=true;
 				}
-				//Avsluta spelet
-				else {sluta();}
-			}
-			//spelare hade fel
-			else {
-				if(start==false)return;
-				
-				//Lägg till fel-poäng
-				f=f+1;
-				
-				//sättes till sann så slumpgeneratorn tidigare inte körs
-				nyttSpel=true;
-				
-				//Presentera fel och statistik
-				alert("Fel! Antal fel: " + f + " Antal rätt: "+ r +" ");//+rand
-				
-				//Gissa igen vid fel
-				gissa = window.prompt("Gissa igen!", "");
-				if(gissa===null){sluta();}
-			}
-
+		//Spelarens gissningar (körs ensamt om nyGiss är true, dvs spelaren kan gissa flera ggr)
+		gissa=inData();
+		if(gissa==null){location.reload(true); return;}
+		
+		//rightOrWrong returnerar sant el falsk, sant=nygissning, falsk=helt nytt spel
+		nyGiss=rightOrWrong(gissa);
+		if(nyGiss==null){location.reload(true); return;}
 		}
 }
-/*Denna fuktion ser till att variabler spelaren matar in inte är under 1 eller över 100,
-inte har lämnat en blankrad och kollar så det är en siffra och inget annat*/
-function kontroll(){
-	
-		//rensa variabler
-		gissa=null;
-		input1=null;
-		input2=null;
-	
-	//Spelarens första inmatning
-	input1 = window.prompt("Välj den första siffran mellan 1-100, tack!", "");
-	if(input1==null)sluta();
-		
-		//Kontrollen
-		if(isNaN(input1) || input1=="" || input1<1 || input1>100){
-			window.alert("Endast siffor mellan 1-100!");
-			
-		//Om kontrollen blev sann ropar metoden på sig själv och allt börjar om	
-		kontroll();
-		
-		//För att bryta aktivitet
-		 return;
-		}
 
-	input2 = window.prompt("Välj den andra siffran mellan 1-100!", "");
-	if(input2==null)sluta();
-		 if(isNaN(input2)|| input2=="" || input2<1 || input2>100){
-			 window.alert("Endast siffor mellan 1-100 tackar!");
-		 kontroll();
-		  return;
-		 }
-
-	gissa = window.prompt("Gissa nu vilken siffra mellan de du valt som slumpats fram!", "");
-	if(gissa===null)sluta();
-		if(isNaN(gissa) || gissa=="" || gissa<1 || gissa>100){
-			window.alert("Endast siffor mellan 1-100 tack!");
-		kontroll();
-		 return;
-		}
-	 return;
-	 
+//Tar emot spelarens gissning och jämför med rand och returnerar nyGiss
+function rightOrWrong(gissa)
+{
+	//Spelaren gissade rätt och får frågan om nytt spel
+	if (gissa===rand){
+					//Nollställa alla variabler
+					gissa=null;
+					input1=null;
+					input2=null;
+					rand=null;
+					//Poäng
+					r=r+1;
+					alert("RÄTT! Antal rätt: " +r +" Antal fel: "+ f +" ");
+					var tmp=window.confirm("Tryck OK för att spela igen!");
+					if(!tmp) {location.reload(true); return;}
+				
+					return false;
+						
+			}
+			//spelaren gissade fel och uppmans gissa igen (nyGiss fortsätter vara true)
+		else{
+			//Lägg till fel-poäng
+			f=f+1;
+			//Presentera fel och statistik
+			alert("Fel! Antal fel: " + f + " Antal rätt: "+ r +" \nrand:"+rand);
+			alert("Gissa igen!");
+		
+			return true;
+				}	
+	
 }
-//Avslutar allt och laddar om sidan så slumptalen inte sparas
-function sluta(){start=false; location.reload(true); return;}
 
 //Slumpgenerator som skickar ett slump tal mellan de två tal som spelaren valde
 function randomIntFromInterval(input1, input2){
-	var t= Math.floor(Math.random()*(input2-input1+1)+input1);
-	if(t<1)return 1;
-	else return t;
+ return Math.floor(Math.random()*(input2-input1+1)+input1);
+ 
 	}
-
+//Tar emot spelarens data och kollar efter diverse fel och returnerar sant/falsk
+function check(inp){
+if(isNaN(inp) || inp=="" || inp<1 || inp>100)
+			return false;
+		else return true;
+		}
+		
+	
+			//Sköter promptar som spelaren ska mata in data i och returnerar input som en Integer
+			function inData(){
+				//anv för att ändra texten 
+				var svar;
+				if(nyGiss) svar="Gissa!";
+				else svar="Välj en siffran mellan 1-100, tack!";
+				
+				var w1=true;
+					while(w1)
+					{
+					//	
+					var inp = window.prompt(svar, "");
+					if(!inp){location.reload(true); return;}
+					//Kolla data som spelaren matade in: om falsk visas en alert och loopen fortsätter
+					if(!check(inp)) {
+					window.alert("Endast siffror mellan 1 och 100 tack! " + inp);
+					w1=true;
+					}
+					else w1=false;
+					}
+					
+					return Number(inp);
+			} 
