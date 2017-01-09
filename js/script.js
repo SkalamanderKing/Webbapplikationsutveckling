140e4f380d255381fa792eb39f561833c45f35c4
SkalamanderKing <skala_mander@yahoo.se>
2017-01-09 15:44:59 +0100

    testing

--- js/script.js
+++ js/script.js
@@ -2,19 +2,17 @@
 
 /*If the user clicks on the Birdy image it swaps between "sleep" and "awake"*/
 $(document).ready(function() {
-    // attaching onclick event listener to all images with class img-responsive
+
     $('.birdy').click(function() {
 		
-        // get current event target (same as 'this')
+
 	     var $this = $('.birdy');
 
 		//attr() method returns the selectors attribute, it is stored in a variable
         var swap = $this.attr('src');
 		
-        // set the attribute src to a new value: the value of rel="" from .img-responsive
         $this.attr('src', $this.attr('rel'));
 		
-        // set rel's value to the old src value
         $this.attr('rel', swap);
     });
 });
