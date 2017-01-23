8d5ba7ef749f89a70dcf696084af6165b80fdb37
SkalamanderKing <skala_mander@yahoo.se>
2017-01-23 20:50:41 +0100

    diverse

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 ﻿/*Fredrick Östlund 2016*/
 
+
+/*Hover effect on images, uses css and changes opacity*/
+$(document).ready(function(){
+
+  $('.img-responsive').hover(
+    function(){
+     $(this).addClass('active');
+	 
+    },
+    function(){
+    $(this).removeClass('active');
+    }
+  );
+
+});
+
+
+
